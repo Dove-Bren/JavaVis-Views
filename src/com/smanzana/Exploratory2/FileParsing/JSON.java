7aec287dfdc58978e07b6f4381b6f79580cc90cf
Skyler <skymanzanares@hotmail.com>
2015-02-22 00:28:44 -0700

    Getting the visual side up!

--- src/com/smanzana/Exploratory2/FileParsing/JSON.java
+++ src/com/smanzana/Exploratory2/FileParsing/JSON.java
@@ -6,24 +6,37 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 
+import com.smanzana.Exploratory2.Graph.DirectedGraph;
+import com.smanzana.Exploratory2.Graph.DirectedGraphNode;
+import com.smanzana.Exploratory2.Graph.DirectedWeightedEdge;
 import com.smanzana.Exploratory2.Tree.Tree;
 
 public final class JSON {
 	
-	public static void toJSON(File outfile, Tree root) {
+	private static boolean prepOutputFile(File outfile) {
 		if (outfile == null || (outfile.exists() && outfile.isDirectory())) {
 			System.err.println("Cannot save json to file, as it either is null or a directory!");
+			return false;
 		}
 		
 		if (outfile.exists()) {
 			System.out.println("Overwriting " + outfile.getAbsolutePath());
+			return true;
 		} else {
 			try {
 				outfile.createNewFile();
+				return true;
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+				return false;
 			}
+		}
+	}
+	
+	public static void toJSON(File outfile, Tree root) {
+		if (!prepOutputFile(outfile)) {
+			return;
 		}
 		
 		PrintWriter writer = null;
@@ -69,6 +82,83 @@ public final class JSON {
 		
 		writer.print("]");
 		writer.print("}");
+	}
+	
+	public static void toJSON(File outfile, DirectedGraph graph) {
+		if (graph == null) {
+			return;
+		}
+
+		if (!prepOutputFile(outfile)) {
+			return;
+		}
+
+		
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(outfile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//no nifty recusion here D:
+		writer.print("[ ");
+		
+		Iterator<DirectedGraphNode> nodeIt = graph.getNodes().iterator();
+		DirectedGraphNode node;
+		while (nodeIt.hasNext()) {
+			node = nodeIt.next();
+			writer.print("{ ");
+			writer.print("\"id\": \"" +
+						  node.getUniqueKey() + 
+						  "\", ");
+			writer.print("\"name\": \"" +
+						  node.getName() + 
+						  "\", ");
+			writer.print("\"data\": { }, ");
+			writer.print("\"adjacencies\": [");
+			
+			//go over all edges
+			Iterator<DirectedWeightedEdge> edgeIt = node.getEdges().iterator();
+			DirectedWeightedEdge edge;
+			while (edgeIt.hasNext()) {
+				edge = edgeIt.next();
+				writer.print("{ ");
+				writer.print("\"nodeTo\": \"" +
+							  edge.getDestination().getUniqueKey() +
+							  "\", ");
+				writer.print("\"data\": {" +
+							  "\"weight\": \"" +
+							  edge.getWeight() +
+							  "\" }");
+				
+				writer.print(" }");
+				if (edgeIt.hasNext()) {
+					writer.print(", ");
+				}
+			}
+			
+			
+			writer.print("]");
+			
+			
+			
+			writer.print("}");
+			
+			if (nodeIt.hasNext()) {
+				writer.print(", ");
+			}
+		}
+		
+		
+		writer.print(" ]");
+		
+		writer.flush();
+		
+		writer.close();
+		
+		
 	}
 	
 }
